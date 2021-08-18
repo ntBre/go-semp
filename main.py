d13f858884d17366ef835799ce8b52e75ae55f36
Brent Westbrook <brentrwestbrook@gmail.com>
2021-08-18 15:34:31 -0500

    add some notes

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+# dig at levmar
+# https://en.wikipedia.org/wiki/Newton%27s_method_in_optimization
+
+# optimize step size in gradient descent
+# https://en.wikipedia.org/wiki/Line_search
+
+# gradient descent itself
+# https://en.wikipedia.org/wiki/Gradient_descent
+
 from dataclasses import dataclass
 import numpy as np
 import io
 
 # parameters to skip optimizing because they are either constants or
-# derived from other parameters
-DERIVED_PARAMS = ['PQN', 'NValence', 'DDN', 'KON', 'EISol']
+# derived from other parameters. Gaussian verified to run without
+# these
+DERIVED_PARAMS = ["PQN", "NValence", "DDN", "KON", "EISol"]
 
-# safe to skip PQN and Nvalence
-# same for DDN through EISol
-
-# I really need the params to be ordered to make them easier to put
-# into matrices, so two parallel lists with params and the
-# corresponding value in the same positions would be ideal, but this
-# complicates parsing since some of the values have multiple values
+# this says step size should be cube root of machine eps, and that
+# machine eps is around 2.2e16, giving 6e-6 for the step
+# https://en.wikipedia.org/wiki/Numerical_differentiation
+STEP_SIZE = 6e-6
 
 @dataclass
 class Atom:
@@ -23,25 +30,6 @@ class Atom:
     def values(self) -> np.ndarray:
         return np.array(self.vals, dtype=np.float64)
         
-
-# just keep duplicating the key !! ex:
-# [... Beta Beta Beta Beta ...]
-# [... -0.3  0.0  0.0  0.0 ...]
-
-# then when I write them, join with comma while the key is the same
-
-# maybe Atom dataclass with Label and Value fields, then return value
-# is a list of Atoms like it is now
-
-# dig at levmar
-# https://en.wikipedia.org/wiki/Newton%27s_method_in_optimization
-
-# optimize step size in gradient descent
-# https://en.wikipedia.org/wiki/Line_search
-
-# gradient descent itself
-# https://en.wikipedia.org/wiki/Gradient_descent
-
 # okay J is definitely a matrix, but there is only one calculation per
 # row. f(xi, B) = yi and Ji = df(xi, B)/dB. so each row is the change
 # in the function value for a given change in the params. so you do
@@ -74,7 +62,7 @@ class Atom:
 # nstruct x nparam in Jt so Jt*J is
 # nstruct x nstruct and y and f(B) are nstruct x 1
 
-# that doesn't work! that's how I got to the transposed form last
+# that doesn"t work! that"s how I got to the transposed form last
 # time! Jt is nstruct x nparam and you have to multiply by nstruct x 1
 # to get an nparam x
 
@@ -84,12 +72,6 @@ class Atom:
 # is n x m, giving nxn in the product and now y is m x 1 or nstruct x
 # 1 as expected
 
-# see this for ideas about step size in qffs and here
-# https://en.wikipedia.org/wiki/Numerical_differentiation
-
-# based on that, h should be the cube root of machine epsilon, which
-# the article puts around 2.2e-16 for doubles = 6e-6
-
 # now the difference between gradient descent and levmar is that
 # instead of solving the equation
 
@@ -97,10 +79,9 @@ class Atom:
 
 # for d and adding that to B, you just do
 
-# B' = -g * Jt * f(B)
+# B" = -g * Jt * f(B)
 
 # where g is gamma or the step size, I think
-
 
 def load_params(filename: str) -> list[Atom]:
     """load semi-empirical parameters from the Gaussian output file
@@ -110,7 +91,7 @@ def load_params(filename: str) -> list[Atom]:
     atom = ""
     atoms = []
     a = -1
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         for line in f:
             if line == " ****\n":
                 atom_label = True
@@ -126,11 +107,11 @@ def load_params(filename: str) -> list[Atom]:
             elif atom_param:
                 params = line.split()
                 for p in params:
-                    p, v = p.split('=')
+                    p, v = p.split("=")
                     if p not in DERIVED_PARAMS:
-                        fields = v.split(',')
-                        if p == 'DCore':
-                            p += '=' + ','.join(fields[0:2])
+                        fields = v.split(",")
+                        if p == "DCore":
+                            p += "=" + ",".join(fields[0:2])
                             fields = fields[2:]
                         for f in fields:
                             atoms[a].keys.append(p)
@@ -148,7 +129,7 @@ def format_params(atoms: list[Atom]) -> str:
             if k != last:
                 if i > 0:
                     ret.write("\n")
-                if 'DCore' not in k:
+                if "DCore" not in k:
                     ret.write(f"{k}={atom.vals[i]}")
                 else:
                     ret.write(f"{k},{atom.vals[i]}")
@@ -159,21 +140,21 @@ def format_params(atoms: list[Atom]) -> str:
     return ret.getvalue()
 
 
-# TODO make this write a real gaussian input file, so it's going to
+# TODO make this write a real gaussian input file, so it"s going to
 # need more args
 def dump_params(atoms: list[Atom], filename: str):
     """dump semi-empirical parameters to filename"""
-    with open(filename, 'w') as out:
+    with open(filename, "w") as out:
         out.write(format_params(atoms))
 
 
 def load_file07(filename: str):
     """load a list of structures from file07"""
     geoms = []
-    with open(filename, 'r') as inp:
+    with open(filename, "r") as inp:
         buf = []
         for line in inp:
-            if '#' not in line:
+            if "#" not in line:
                 fields = line.split()
                 buf.extend([float(x) for x in fields])
             elif len(buf) > 0:
@@ -186,13 +167,16 @@ def load_file07(filename: str):
 def load_energies(filename: str) -> np.array:
     """load relative ab initio energies from filename"""
     ret = []
-    with open(filename, 'r') as inp:
+    with open(filename, "r") as inp:
         for line in inp:
             ret.append(float(line))
     return np.array(ret, dtype=np.float64)
 
+# TODO write all the gaussian files and corresponding pbs files for a
+# set of params
+
 
 B0 = load_params("opt.out")
 dump_params(B0, "test.out")
-g = load_file07('file07')
-e = load_energies('rel.dat')
+g = load_file07("file07")
+e = load_energies("rel.dat")
