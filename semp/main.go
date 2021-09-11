c03440e66995082c2f2ed492fb6f0d8e7889b4ee
Brent Westbrook <brentrwestbrook@gmail.com>
2021-09-11 16:15:17 -0500

    write gaussian input file

--- semp/main.go
+++ semp/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"text/template"
 )
 
 var (
@@ -17,6 +18,8 @@ var (
 		"KON":      {},
 		"EISol":    {},
 	}
+	CHARGE = 0
+	SPIN   = 1
 )
 
 func toFloat(strs []string) []float64 {
@@ -33,6 +36,7 @@ func toFloat(strs []string) []float64 {
 
 func LoadGeoms(filename string) (ret [][]float64) {
 	f, err := os.Open(filename)
+	defer f.Close()
 	if err != nil {
 		panic(err)
 	}
@@ -56,6 +60,7 @@ func LoadGeoms(filename string) (ret [][]float64) {
 
 func LoadEnergies(filename string) (ret []float64) {
 	f, err := os.Open(filename)
+	defer f.Close()
 	if err != nil {
 		panic(err)
 	}
@@ -85,6 +90,7 @@ type Param struct {
 // output file
 func LoadParams(filename string) (ret []Param) {
 	f, err := os.Open(filename)
+	defer f.Close()
 	if err != nil {
 		panic(err)
 	}
@@ -140,8 +146,13 @@ func LoadParams(filename string) (ret []Param) {
 }
 
 func DumpParams(params []Param, filename string) {
+	f, err := os.Create(filename)
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
 	for _, param := range params {
-		fmt.Printf(" ****\n%2s", param.Atom)
+		fmt.Fprintf(f, " ****\n%2s", param.Atom)
 		var last, sep string
 		for i, name := range param.Names {
 			if strings.Contains(name, "DCore") {
@@ -150,24 +161,59 @@ func DumpParams(params []Param, filename string) {
 				sep = "="
 			}
 			if name == last {
-				fmt.Printf(",%.10f", param.Values[i])
+				fmt.Fprintf(f, ",%.10f", param.Values[i])
 			} else {
-				fmt.Printf("\n%s%s%.10f", name, sep, param.Values[i])
+				fmt.Fprintf(f, "\n%s%s%.10f",
+					name, sep, param.Values[i])
 				last = name
 			}
 		}
-		fmt.Print("\n")
+		fmt.Fprint(f, "\n")
 	}
-	fmt.Println(" ****")
+	fmt.Fprintln(f, " ****")
 }
 
-func WriteCom() {}
+func WriteCom(names []string, coords []float64, paramfile, filename string) {
+	f, err := os.Create(filename)
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	var geom strings.Builder
+	for i := range names {
+		fmt.Fprintf(&geom, "%s%20.12f%20.12f%20.12f\n",
+			names[i], coords[i], coords[i+1], coords[i+2],
+		)
+	}
+
+	t, err := template.New("com").Parse(`%mem=1000mb
+%nprocs=1
+#P PM6=(print,zero)
+
+the title
+
+{{.Charge}} {{.Spin}}
+{{.Geom}}
+@{{.Params}}
+
+`)
+	if err != nil {
+		panic(err)
+	}
+	t.Execute(f, struct {
+		Charge int
+		Spin   int
+		Geom   string
+		Params string
+	}{CHARGE, SPIN, geom.String(), paramfile})
+}
 
 func main() {
 	labels := []string{"C", "C", "C", "H", "H"}
 	fmt.Println(labels)
-	LoadGeoms("file07")
+	geoms := LoadGeoms("file07")
 	LoadEnergies("rel.dat")
 	params := LoadParams("opt.out")
 	DumpParams(params, "params.dat")
+	WriteCom(labels, geoms[0], "params.dat", "out")
 }
