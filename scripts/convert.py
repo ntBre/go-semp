278272561069e7d6d3a070854e1ca90650e487fc
Brent Westbrook <brentrwestbrook@gmail.com>
2022-02-25 13:29:26 -0600

    add python script for converting parameter types, but it's hard

---
+++ scripts/convert.py
@@ -0,0 +1,42 @@
+#                    MOPAC                 Gaussian
+#              USS,UPP,UDD                        U
+#                 ZS,ZP,ZD              ZetaOverlap
+#              ZSN,ZPN,ZDN                   Zeta1C
+#        BetaS,BetaP,BetaD                     Beta
+#         GSS,GPP,…,FODD,…               F0ss,F0pp,
+#                      DD2            DDN=0,1,Value
+#                      DD3            DDN=1,1,Value
+#                      DD4            DDN=0,2,Value
+#                      DD5            DDN=1,2,Value
+#                      DD6            DDN=2,2,Value
+#                      PO1          KON=0,0,0,Value
+#                      PO2          KON=1,0,1,Value
+#                      PO3          KON=2,1,1,Value
+#                      PO4          KON=2,0,2,Value
+#                      PO5          KON=1,1,2,Value
+#                      PO6          KON=2,2,2,Value
+#                      PO7          KON=0,1,1,Value
+#                      PO8          KON=0,2,2,Value
+#                      PO9                   CoreKO
+#                    EHeat                    EHeat
+#                    EISol                    EISol
+#          AlpB_NN,XFac_NN    DCore=NN,3,Alpha,XFac
+#  ****
+#  H
+# F0ss=0.4347370795
+# ZetaOverlap=1.2211262761
+# U=-0.3518832301
+# Beta=-0.5155784907
+# CoreKO=0.8013286314
+#  ****
+#  C
+# F0ss=0.3297999964
+# F0sp=0.4599748643
+# F0pp=0.5068979816
+# F2pp=0.4629935835
+# G1sp=0.0854298260
+# ZetaOverlap=1.8902515109,1.5394196734
+# U=-1.6603016489,-1.7485225317
+# Beta=-0.0927234436,-0.3271224933
+# CoreKO=1.5608664513
+#  ****
